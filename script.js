2d4f4ff21716dad7481bbdbbc14584cffdc17d6f
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-18 08:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function volume_sphere() {
 	let radius = document.getElementById("radius").value;
 	let volume = document.getElementById("volume");
 	let vol = (4/3)*3.14*Math.pow(radius,3);
-	volume.textContent = vol;
+	volume.value = vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
